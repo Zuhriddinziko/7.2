536ff9d988e018136b0f1b3b04ad227a05423dc8
Your Name <your_email@example.com>
2024-12-20 08:20:11 +0500

    xato togrilandi

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -2,12 +2,13 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/config";
 export function useLogin() {
   const loginWithEmailAndPassword = (email, password) => {
+    console.log(email, password);
     signInWithEmailAndPassword(auth, email, password)
-      .then((profile) => {
-        console.log(profile.user);
+      .then((user) => {
+        console.log(user);
       })
       .catch((error) => {
-        console.log(error.massage);
+        console.log(error);
       });
   };
 
--- src/hooks/useRegister.jsx
+++ src/hooks/useRegister.jsx
@@ -2,7 +2,17 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/config";
 export function useRegister() {
   const registerWithEmailAndPassword = (displayName, email, password) => {
-    console.log(displayName, email, password);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
   };
 
   return { registerWithEmailAndPassword };
