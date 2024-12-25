90cd8c9277669e896555395e08d57a91f18239c4
Your Name <your_email@example.com>
2024-12-25 08:14:55 +0500

    ozgarish

--- src/components/OnlineUser.jsx
+++ src/components/OnlineUser.jsx
@@ -4,13 +4,13 @@ function OnlineUser() {
   const { document } = useCollektion("users");
 
   return (
-    <ul className="w-[400px] h-screen bg-stone-400 p-10 flex flex-col gap-2">
+    <ul className="w-[400px] h-screen bg-stone-400 p-10 flex flex-col gap-2 shadow-lg shadow-slate-600">
       {document &&
         document.map((doc) => {
           return (
             <li
               key={doc.id}
-              className="w-[280px] h-[60px] text-black flex items-center rounded-lg p-2  bg-slate-50  "
+              className="w-[280px] h-[60px] text-black flex items-center rounded-lg p-2  bg-slate-50  shadow-slate-600 shadow-lg "
             >
               <div className="flex items-center gap-3">
                 <div className="avatar online">
